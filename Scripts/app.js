f22b35c04c07bc273816059bec5e16087c44447f
Tyler Peterson <tylermpeterson36@gmail.com>
2016-03-23 21:38:41 -0600

    updated css

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,11 +1,10 @@
 $('.close-button').click(function(e) {
-	e.preventDefault();
-	$('#scrim').css("display","none");
-	$('#panopresentation').css("display","none");
-
+    e.preventDefault();
+    $('#scrim').css("display", "none");
+    $('#panopresentation').css("display", "none");
 });
 
 $('#Generate').click(function() {
-	$('#scrim').css("display","block");
-	$('#panopresentation').css("display","block");
+    $('#scrim').css("display", "block");
+    $('#panopresentation').css("display", "block");
 })
