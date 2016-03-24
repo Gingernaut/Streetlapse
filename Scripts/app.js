d9c4e6189da8c2db444cbe47f6155b1b99422124
Tyler Peterson <tylermpeterson36@gmail.com>
2016-03-24 11:45:41 -0600

    navbar added and modals updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,11 +1,19 @@
 $('.close-button').click(function(e) {
     e.preventDefault();
     $('#scrim').css("display", "none");
-    $('#panopresentation').css("display", "none");
+    $('.modal').css("display", "none");
+    $('#aboutme').css("display","none");
     window.location.reload();
 });
 
 $('#Generate').click(function() {
     $('#scrim').css("display", "block");
-    $('#panopresentation').css("display", "block");
+    $('#mapmodal').css("display", "block");
+});
+
+
+$('.svg-wrapper').click(function(e) {
+	e.preventDefault();
+    $('#scrim').css("display", "block");
+    $('#aboutme').css("display","block");
 });
